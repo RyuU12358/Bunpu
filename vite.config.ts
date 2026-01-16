9bb9f096bb166d12509a79f05d3c2283aabadc86
RyuU12358 <tqanothume@gmail.com>
2026-01-17 00:02:00 +0900

    Fix vite worker format

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vite/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/Bunpu/",
+  worker: {
+    format: "es",
+  },
 });
